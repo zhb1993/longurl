2b527f5541c95949de0a88993a75477573ae7655
hongbin.zhang <976109421@qq.com>
2022-09-08 05:45:58 +0800

    初始化

--- src/main/kotlin/com/zhb/longurl/LongUrlApplication.kt
+++ src/main/kotlin/com/zhb/longurl/LongUrlApplication.kt
@@ -1,9 +1,11 @@
 package com.zhb.longurl
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@MapperScan("com.zhb.longurl.mapper")
 class LongUrlApplication
 
 fun main(args: Array<String>) {
