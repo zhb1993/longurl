f6f8ad3a530cfd69875c2e2f5efc6ffe6d66e5d0
hongbin.zhang <976109421@qq.com>
2022-09-09 06:54:39 +0800

    提交一点点

--- src/main/kotlin/com/zhb/longurl/service/UrlService.kt
+++ src/main/kotlin/com/zhb/longurl/service/UrlService.kt
@@ -1,12 +1,34 @@
 package com.zhb.longurl.service
 
+import com.alibaba.fastjson.JSON
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
 import com.zhb.longurl.entity.Url
 import com.zhb.longurl.mapper.UrlMapper
 import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
 
 @Slf4j
 @Service
 class UrlService : ServiceImpl<UrlMapper, Url>() {
+
+    @Autowired
+    lateinit var redisTemplate: RedisTemplate<String, Any>
+
+    override fun save(url: Url): Boolean {
+        var sourceUrl = url.sourceUrl
+        val target: String = ""
+        val urlCount = redisTemplate.opsForValue().get("url:count")
+        if ("" == urlCount){
+            redisTemplate.opsForValue().set("url:count",0)
+        }
+
+
+
+        url.targetUrl = target
+        redisTemplate.opsForValue().set(target,url)
+
+        return true
+    }
 }
---
+++ src/main/kotlin/com/zhb/longurl/utils/ZUtil.kt
@@ -0,0 +1,5 @@
+package com.zhb.longurl.utils
+
+class ZUtil {
+
+}
